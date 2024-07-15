1fe7f11bab2fa28825549ea8b9136f1229633e11
ASujay <aniketsujay110@gmail.com>
2024-07-15 10:24:46 +0900

    Rewrite

--- main.go
+++ main.go
@@ -1,23 +1,8 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/nsf/termbox-go"
-)
+import "fmt"
 
 func main() {
-    err := termbox.Init()
-    if err != nil {
-        fmt.Println("Failed to initialize termbox: ", err)
-        os.Exit(1)
-    }
-    program:= tea.NewProgram(InitEditor())
-    if _, err := program.Run(); err != nil {
-        fmt.Printf("Error: %v", err)
-        os.Exit(1)
-    }
-    termbox.Close()
+    fmt.Println("Hello World")
 }
+
